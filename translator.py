1402854f01cc024b0bd0252f14c226b2f5ec72aa
pauytramosio <pauytramosio@gmail.com>
2025-03-21 14:53:39 +0200

    dechila;ke

--- translator.py
+++ translator.py
@@ -15,11 +15,13 @@ def translate(tokens: list[list[Token]], indent=4) -> str:
     while i < len(tokens):
         while j < len(tokens[i]):
             if isinstance(tokens[i][j], Keyword):
-                match type(tokens[i][j]):
-                    case ExitKeyword:
+                if isinstance(tokens[i][j], ExitKeyword):
                         include.add("stdlib.h")
                         mainf += f"{indent * ' '}exit({tokens[i][j+1].value});\n"
                         j += 2
+                elif isinstance(tokens[i][j], LetKeyword):
+                        mainf += f"{tokens[i][j+2].value} = {tokens[i][j+4].value};\n"
+                        j += 5
         i += 1
     
     return (mainf, include)
