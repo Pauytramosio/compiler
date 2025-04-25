3e7d389ae910a91ea715735d0ffc0b7cf6caac20
Pauytramosio <pauytramosio@gmail.com>
2025-04-25 16:52:10 +0300

    Added comment support.

---
+++ docs/help.md
@@ -0,0 +1,6 @@
+# Sabbah Help Page
+
+Welcome to the Sabbah Help Page! Here, you will find techincal help on Sabbah. (For help about the Sabbah compiler CLI, see `../help.txt`.)
+
+## Sources
+[GitHub](https://github.com/Pauytramosio/compiler.git)
---
+++ pre.py
@@ -0,0 +1,22 @@
+def remove_comments(input_string: str) -> str:
+    result = []
+    i = 0
+    length = len(input_string)
+    while i < length:
+        if i + 1 < length and input_string[i:i+2] == '/*':
+            i += 2
+            while i + 1 < length and input_string[i:i+2] != '*/':
+                i += 1
+            if i + 1 < length:
+                i += 2
+            continue
+        if i + 1 < length and input_string[i:i+2] == '//':
+            i += 2
+            while i < length and input_string[i] != '\n':
+                i += 1
+            continue
+        result.append(input_string[i])
+        i += 1
+    return ''.join(result)
+
+print(remove_comments("exit 0; /* comment */"))
